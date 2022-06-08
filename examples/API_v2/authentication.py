45166f53a7c17d028eeab6505119eac523b9f2e6
ezellmt <ezellmt@alumni.lipscomb.edu>
2022-06-08 15:29:10 -0500

    Committ

--- examples/API_v2/authentication.py
+++ examples/API_v2/authentication.py
@@ -4,22 +4,22 @@ import tweepy
 # of the Keys and Tokens tab of your app, under the
 # Twitter Developer Portal Projects & Apps page at
 # https://developer.twitter.com/en/portal/projects-and-apps
-bearer_token = ""
+bearer_token = "AAAAAAAAAAAAAAAAAAAAABY4TwEAAAAAKRHBh0JJ0YHjlGLYBAkeHH6lb%2BQ%3DiAERZ0SmmAqz5emb8RLU10sKekwnTa0EvhVCfWZaCdnYzcoqBy"
 
 # Your app's API/consumer key and secret can be found under the Consumer Keys
 # section of the Keys and Tokens tab of your app, under the
 # Twitter Developer Portal Projects & Apps page at
 # https://developer.twitter.com/en/portal/projects-and-apps
-consumer_key = ""
-consumer_secret = ""
+consumer_key = "n7a9m8C1WW7PMyAb6SU2RQOAS"
+consumer_secret = "pxdm1vDd5fEDWGaHq0IhQHPvBUgsJIRjXUzzBWdEpcfZ4ENyvc"
 
 # Your account's (the app owner's account's) access token and secret for your
 # app can be found under the Authentication Tokens section of the
 # Keys and Tokens tab of your app, under the
 # Twitter Developer Portal Projects & Apps page at
 # https://developer.twitter.com/en/portal/projects-and-apps
-access_token = ""
-access_token_secret = ""
+access_token = "90438849-lB47pWSL0KodQ7CtMtFQwNxomrpczO5w3F3zbULIh"
+access_token_secret = "87kRfLEDQbBUaoKQw6f4Zjo38hZNhPe0NecxxrJFYFApy"
 
 # You can authenticate as your app with just your bearer token
 client = tweepy.Client(bearer_token=bearer_token)
